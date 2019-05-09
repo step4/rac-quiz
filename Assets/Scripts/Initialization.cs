065e0be1fa43f09ca17a81d504c838bbacac5add
step4 <c.rausch020@gmail.com>
2019-05-09 19:56:26 +0200

    strip parse sdk

--- Assets/Scripts/Initialization.cs
+++ Assets/Scripts/Initialization.cs
@@ -2,33 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Reflection;
-using Parse;
+using UnityEngine.Networking;
+using System.Net.Http;
+using TMPro;
 
 public class Initialization : MonoBehaviour
 {
+    public TextMeshProUGUI text;
     // Start is called before the first frame update
     void Start()
     {
-        ParseClient.Initialize(new ParseClient.Configuration
-        {
-            ApplicationID = "hochschulQuiz",
-            Key = "",
-            ServerURI = @"http://localhost:8080/api"
-        });
-
-        //IDictionary<string, object> param = new Dictionary<string, object>
-        //{
-        //    { "platyerName", "friend" }
-        //};
-        //ParseCloud.CallFunctionAsync<IDictionary<string, object>>("averageStars", param).ContinueWith(t =>
-        //{
-        //    Debug.Log(t);
-        //});
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    public async void loadit()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://loripsum.net/api");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Debug.Log(responseBody);
+                text.text = responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                Debug.Log("\nException Caught!");
+                Debug.Log(e.Message);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/StartScreenViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class StartScreenViewModel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ParseClientGO;
+
+    private IWebClient parseClient;
+
+    private void Awake()
+    {
+        parseClient = ParseClientGO.GetComponent<IWebClient>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        parseClient.Send();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
