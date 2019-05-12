7256f9ce7e9abf97c34818a0ee5fb9f008c03778
Chris <c.rausch020@gmail.com>
2019-05-12 21:53:48 +0200

    avataaars preperation

---
+++ Assets/Scripts/Model/Avataaars/Avataaars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AvataaarsOptionEnums;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+
+public class Avataaars
+{
+    public Avataaars()
+    {
+        
+    }
+    public List<string> GetEnums()
+    {
+        var nameSpace = "AvataaarsOptionEnums";
+        Assembly asm = Assembly.GetExecutingAssembly();
+        return asm.GetTypes()
+            .Where(type => type.Namespace == nameSpace)
+            .Select(type => type.Name).ToList();
+    }
+
+}
---
+++ Assets/Scripts/Model/Avataaars/OptionEnums.cs
@@ -0,0 +1,219 @@
+﻿namespace AvataaarsOptionEnums
+{
+    public enum AvatarStyle
+    {
+        Circle,
+        Transparent,
+    }
+
+    public enum TopType
+    {
+        NoHair,
+        Eyepatch,
+        Hat,
+        Hijab,
+        Turban,
+        WinterHat1,
+        WinterHat2,
+        WinterHat3,
+        WinterHat4,
+        LongHairBigHair,
+        LongHairBob,
+        LongHairBun,
+        LongHairCurly,
+        LongHairCurvy,
+        LongHairDreads,
+        LongHairFrida,
+        LongHairFro,
+        LongHairFroBand,
+        LongHairNotTooLong,
+        LongHairShavedSides,
+        LongHairMiaWallace,
+        LongHairStraight,
+        LongHairStraight2,
+        LongHairStraightStrand,
+        ShortHairDreads01,
+        ShortHairDreads02,
+        ShortHairFrizzle,
+        ShortHairShaggyMullet,
+        ShortHairShortCurly,
+        ShortHairShortFlat,
+        ShortHairShortRound,
+        ShortHairShortWaved,
+        ShortHairSides,
+        ShortHairTheCaesar,
+        ShortHairTheCaesarSidePart,
+    }
+
+    public enum AccessoriesType
+    {
+        Blank,
+        Kurt,
+        Prescription01,
+        Prescription02,
+        Round,
+        Sunglasses,
+        Wayfarers,
+    }
+
+    public enum HairColor
+    {
+        Auburn,
+        Black,
+        Blonde,
+        BlondeGolden,
+        Brown,
+        BrownDark,
+        PastelPink,
+        Platinum,
+        Red,
+        SilverGray,
+    }
+
+    public enum HatColor
+    {
+        Black,
+        Blue01,
+        Blue02,
+        Blue03,
+        Gray01,
+        Gray02,
+        Heather,
+        PastelBlue,
+        PastelGreen,
+        PastelOrange,
+        PastelRed,
+        PastelYellow,
+        Pink,
+        Red,
+        White,
+    }
+
+    public enum FacialHairType
+    {
+        Blank,
+        BeardMedium,
+        BeardLight,
+        BeardMagestic,
+        MoustacheFancy,
+        MoustacheMagnum,
+    }
+
+    public enum FacialHairColor
+    {
+        Auburn,
+        Black,
+        Blonde,
+        BlondeGolden,
+        Brown,
+        BrownDark,
+        Platinum,
+        Red,
+    }
+
+    public enum ClotheType
+    {
+        BlazerShirt,
+        BlazerSweater,
+        CollarSweater,
+        GraphicShirt,
+        Hoodie,
+        Overall,
+        ShirtCrewNeck,
+        ShirtScoopNeck,
+        ShirtVNeck,
+    }
+
+    public enum ClotheColor
+    {
+        Black,
+        Blue01,
+        Blue02,
+        Blue03,
+        Gray01,
+        Gray02,
+        Heather,
+        PastelBlue,
+        PastelGreen,
+        PastelOrange,
+        PastelRed,
+        PastelYellow,
+        Pink,
+        Red,
+        White,
+    }
+
+    public enum GraphicType
+    {
+        Bat,
+        Cumbia,
+        Deer,
+        Diamond,
+        Hola,
+        Pizza,
+        Resist,
+        Selena,
+        Bear,
+        SkullOutline,
+        Skull,
+    }
+
+    public enum EyeType
+    {
+        Close,
+        Cry,
+        Default,
+        Dizzy,
+        EyeRoll,
+        Happy,
+        Hearts,
+        Side,
+        Squint,
+        Surprised,
+        Wink,
+        WinkWacky,
+    }
+
+    public enum EyebrowType
+    {
+        Angry,
+        AngryNatural,
+        Default,
+        DefaultNatural,
+        FlatNatural,
+        RaisedExcited,
+        RaisedExcitedNatural,
+        SadConcerned,
+        SadConcernedNatural,
+        UnibrowNatural,
+        UpDown,
+        UpDownNatural,
+    }
+
+    public enum MouthType
+    {
+        Concerned,
+        Default,
+        Disbelief,
+        Eating,
+        Grimace,
+        Sad,
+        ScreamOpen,
+        Serious,
+        Smile,
+        Tongue,
+        Twinkle,
+        Vomit,
+    }
+
+    public enum SkinColor
+    {
+        Tanned,
+        Yellow,
+        Pale,
+        Light,
+        Brown,
+        DarkBrown,
+        Black,
+    }
+}
