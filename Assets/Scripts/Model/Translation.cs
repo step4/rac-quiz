48a513578e57ea2fa1a473cc7e4f8c5cb2aa8359
Chris <c.rausch020@gmail.com>
2019-07-03 14:47:11 +0200

    avataaars translation

---
+++ Assets/Scripts/Model/Translation.cs
@@ -0,0 +1,10 @@
+﻿
+using System;
+
+[Serializable]
+public class Translation
+{
+    public string original;
+    public string translated;
+}
+
---
+++ Assets/Scripts/ScriptableObjects/TranslationAvataaarsSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TranslationAvataaarsSO : ScriptableObject
+{
+    public List<Translation> translation;
+}
